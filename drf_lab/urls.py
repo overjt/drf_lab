d0782b6c91521242ed641ef86a69d1ecd8562409
Jonathan Toledo <kry0ster@gmail.com>
2019-07-05 23:44:07 -0500

    Basic rest for Persons

--- drf_lab/urls.py
+++ drf_lab/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
+from rest_framework import routers
+from persons.views import PersonViewSet
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'persons', PersonViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^', include(router.urls))
 ]
--- persons/views.py
+++ persons/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Person
+from .serializers import PersonSerializer
 
-# Create your views here.
+# ViewSets define the view behavior.
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
